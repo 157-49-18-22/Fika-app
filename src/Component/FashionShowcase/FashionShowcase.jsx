d65dc1a3d5960c825180a479cdecba7be81cfccc
Ayush3323 <ayusht8799@gmail.com>
2025-04-24 16:43:18 +0530

    updated homepage

--- src/Component/FashionShowcase/FashionShowcase.jsx
+++ src/Component/FashionShowcase/FashionShowcase.jsx
@@ -52,7 +52,11 @@ const FashionShowcase = () => {
               <div className="info-card-header">
                 <img src="./image-icon.png" alt="img" />
                 <span
-                  style={{ paddingRight: "10px", textTransform: "capitalize" }}
+                  style={{
+                    paddingRight: "10px",
+                    textTransform: "capitalize",
+                    fontWeight: "500",
+                  }}
                 >
                   cushion collection
                 </span>
@@ -62,7 +66,6 @@ const FashionShowcase = () => {
                   style={{
                     borderBottom: "2px solid #1D1D1D",
                     fontSize: "33px",
-                    // fontFamily: "Montserrat",
                     fontWeight: "500",
                     textTransform: "capitalize",
                     paddingBottom: "20px",
@@ -70,14 +73,119 @@ const FashionShowcase = () => {
                 >
                   express your through style
                 </p>
-                <p style={{ padding: "10px" }}>
+                <p
+                  style={{
+                    padding: "10px",
+                    fontWeight: "500",
+                    paddingBottom: "0px",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "10px",
+                  }}
+                >
                   Elevete your style with our fashion finds
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="black"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    style={{ transform: "rotate(-45deg)" }}
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7" />
+                  </svg>
                 </p>
               </div>
             </div>
           </div>
           <div style={{ width: "50%" }}>
-            <img src="./side_image1.png" alt="img" />
+            <div style={{ position: "relative" }}>
+              <img src="./side_image1.png" alt="img" />
+              <div
+                style={{
+                  position: "absolute",
+                  top: "20px",
+                  right: "20px",
+                  background: "white",
+                  borderRadius: "50%",
+                  width: "40px",
+                  height: "40px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  style={{ transform: "rotate(-45deg)" }}
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7" />
+                </svg>
+              </div>
+              <div
+                style={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: "20px",
+                }}
+              >
+                <div
+                  style={{
+                    background: "rgba(255, 255, 255, 0.7)",
+                    borderRadius: "20px",
+                    padding: "30px 20px",
+                    position: "relative",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "inline-block",
+                      background: "white",
+                      padding: "8px 16px",
+                      borderRadius: "20px",
+                      fontSize: "8px",
+                      fontWeight: "600",
+                      textTransform: "uppercase",
+                      marginBottom: "20px",
+                    }}
+                  >
+                    CUSHION COLLECTION
+                  </div>
+                  <h2
+                    style={{
+                      fontSize: "28px",
+                      fontWeight: "500",
+                      lineHeight: "1.1",
+                      color: "#1D1D1D",
+                    }}
+                  >
+                    Unlock Your
+                    <br />
+                    Fashion
+                    <br />
+                    Potential!!!
+                  </h2>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/Component/StyleEmpower/StyleEmpower.jsx
+++ src/Component/StyleEmpower/StyleEmpower.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './StyleEmpower.css'
+import React from "react";
+import "./StyleEmpower.css";
 
 const StyleEmpower = () => {
   return (
@@ -7,15 +7,29 @@ const StyleEmpower = () => {
       <div className="style-empower-content">
         <div className="style-text-container">
           <div className="text-row first-row">
-            <h2 className="style-text">YOUR STYLE <span className="style-image-marker right"></span></h2>
+            <h2 className="style-text">
+              YOUR STYLE
+              <img
+                src="/textured_image1.png"
+                alt="Texture 1"
+                className="textured-image"
+              />
+            </h2>
           </div>
           <div className="text-row second-row">
-            <h2 className="style-text"><span className="style-image-marker left"></span> YOUR RULES</h2>
+            <h2 className="style-text">
+              <img
+                src="/textured_image2.png"
+                alt="Texture 2"
+                className="textured-image"
+              />
+              YOUR RULES
+            </h2>
           </div>
           <div className="text-row third-row">
             <h1 className="empower-text">EMPOWER YOUR FASHION</h1>
           </div>
-          
+
           <div className="button-container">
             <button className="shop-now-button">
               <span>SHOP NOW</span>
@@ -25,7 +39,7 @@ const StyleEmpower = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StyleEmpower 
+export default StyleEmpower;
