64bf934fb6cbaebd095ee1e6f2eb871a2912d382
Ayush3323 <ayusht8799@gmail.com>
2025-04-25 19:25:25 +0530

    update navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import { FaSearch, FaShoppingCart, FaHeart, FaBars } from "react-icons/fa";
+import { FiShoppingBag } from "react-icons/fi";
 import { Link, NavLink } from "react-router-dom";
 import { useCart } from "../../context/CartContext.jsx";
 import { useWishlist } from "../../context/WishlistContext.jsx";
@@ -17,55 +18,33 @@ function Navbar() {
         </button> */}
 
         <div className="navbar-left">
-          <NavLink to="/new-arrivals" className="nav-link">
-            New Arrival
-          </NavLink>
-          <NavLink to="/all-products" className="nav-link">
-            All Product
-          </NavLink>
-          <NavLink to="/blog" className="nav-link">
-            Blog
-          </NavLink>
-          <NavLink to="/contact" className="nav-link">
-            Contact Us
-          </NavLink>
+          <NavLink to="/new-arrivals" className="nav-link">New Arrival</NavLink>
+          <NavLink to="/all-products" className="nav-link">All Product</NavLink>
+          <NavLink to="/blog" className="nav-link">Blog</NavLink>
+          <NavLink to="/contact" className="nav-link">Contact Us</NavLink>
         </div>
 
         <div className="navbar-center">
-          <Link to="/" className="logo">
-            Fika
-          </Link>
+          <Link to="/" className="logo">Fika</Link>
         </div>
 
         <div className="navbar-right">
           <div className="search-bar">
             <input type="text" placeholder="Search Product..." />
-            <button className="search-button">
-              <FaSearch />
-            </button>
+            <button className="search-btn"><FaSearch /></button>
           </div>
-          <div className="header-actions">
-            <Link
-              to="/wishlist"
-              className="action-btn wishlist-btn"
-              aria-label="Wishlist"
-            >
-              <FaHeart />
-              {wishlist.length > 0 && (
-                <span className="badge">{wishlist.length}</span>
-              )}
-            </Link>
-            <Link
-              to="/cart"
-              className="action-btn cart-btn"
-              aria-label="Shopping Cart"
-            >
-              <FaShoppingCart />
-              {getCartCount() > 0 && (
-                <span className="badge">{getCartCount()}</span>
-              )}
-            </Link>
-          </div>
+          <Link to="/wishlist" className="" aria-label="Wishlist">
+            <FaHeart style={{ color: '#333' }} />
+            {wishlist.length > 0 && (
+              <span className="badge">{wishlist.length}</span>
+            )}
+          </Link>
+          <Link to="/cart" className="" aria-label="Shopping Cart">
+            <FiShoppingBag style={{ color: '#333' }} />
+            {getCartCount() > 0 && (
+              <span className="badge">{getCartCount()}</span>
+            )}
+          </Link>
         </div>
       </div>
     </div>
