30ca20d29003d62d0102eb34e9b4fd87fff91a70
abhi <akay13230@gmail.com>
2025-05-06 16:35:24 +0530

    Update styles in FashionShowcase, Navbar, and StyleEmpower components
    - Adjusted text content in FashionShowcase for improved clarity.
    - Reduced font size for the logo in Navbar and logo-watch for better visual balance.
    - Increased gap in StyleEmpower component to enhance layout spacing.

--- src/Component/FashionShowcase/FashionShowcase.jsx
+++ src/Component/FashionShowcase/FashionShowcase.jsx
@@ -84,7 +84,7 @@ const FashionShowcase = () => {
                 >
                   Elevete your style with our fashion finds
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns="http://www.w3.org/2000/svg"  
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
